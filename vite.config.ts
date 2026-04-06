8bb1e8529eab2b78bafbc92180b062e33a9d84a5
unknown <lucasmeloil@pm.me>
2026-04-05 22:25:15 -0300

    feat(security): implementa cabeçalhos de segurança e endurecimento do build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false, // Oculta o código fonte original no F12 em produção
+    minify: 'esbuild', // Reduz e ofusca o código
+    chunkSizeWarningLimit: 1600,
+  }
 })
